29cf639079c5aaba6ee64dacc6cc5dcc242dc6bb
Karina <karina.stala@gmail.com>
2023-05-25 21:12:56 +0200

    modified

--- todo-list.js
+++ todo-list.js
@@ -12,16 +12,28 @@ todoList.forEach((todoObject, index) => {
   const html = `
   <div>${name}</div> 
   <div>${dueDate}</div>
-  <button onclick="
-  todoList.splice(${index}, 1);
-  renderTodoList();
-  "class="delete-todo-button">Delete</button>`;
+  <button class="delete-todo-button js-delete-todo-button">Delete</button>`;
   todoListHTML += html;
 });
 
   document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+  .forEach((deleteButton, index)=> {
+  deleteButton.addEventListener('click', ()=>{
+  todoList.splice(index, 1);
+  renderTodoList();
 }
+);
+  });
+}
+
+  document.querySelector(".js-add-task-button")
+  .addEventListener("click", () => {
+  addTask();
+}
+);
 
 function addTask () {
  const inputElement = document.querySelector('.js-name-input');
